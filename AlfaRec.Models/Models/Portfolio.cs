e2b15f30d1d8be7e03f880b7903163b964dfd332
Marcin Radziszewski <marcincho@gmail.com>
2019-10-25 20:19:17 +0200

    initial template

---
+++ AlfaRec.Models/Models/Portfolio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundConnRec.Models.Models
+{
+    public class Portfolio
+    {
+    }
+}
---
+++ AlfaRec/Models/FundConnContext.cs
@@ -0,0 +1,22 @@
+﻿using FundConnRec.Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FundConnRec.API.Models
+{
+    public class FundConnContext : DbContext
+    {
+        public FundConnContext(DbContextOptions<FundConnContext> options) : base(options)
+        {
+
+        }
+        public FundConnContext()
+        {
+
+        }
+        public virtual DbSet<Portfolio> Portfolios { get; set; }
+
+        public virtual DbSet<Position> Positions { get; set; }
+
+        public virtual DbSet<Security> Securities { get; set; }
+    }
+}
